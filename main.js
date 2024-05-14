59d51840009d5bc88d442bd3ad1829eb9ed555e6
Prabjot Singh <125903944+singh1733@users.noreply.github.com>
2024-05-14 17:00:55 -0400

    Added event listeners for number buttons
    This allows for the button presses to be registered and displayed on the screen or, in the fture, be used in calculations.

--- main.js
+++ main.js
@@ -6,24 +6,48 @@ function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1,num2){
-    return num1*num2;
-} 
+function multiply(num1, num2) {
+    return num1 * num2;
+}
 
-function divide(num1,num2){
-    return num1/num2;
+function divide(num1, num2) {
+    return num1 / num2;
 }
 
 let num1, num2, operator, result;
 
-function operate(num1,num2, operator){
-    if (operator=="/"){
-        result=divide(num1,num2);
-    }else if (operator=="*"){
-        result=multiply(num1,num2);
-    }else if (operator=="+"){
-        result=add(num1,num2);
-    }else if(operator=="-"){
-        result=subtract(num1,num2);
+function operate(num1, num2, operator) {
+    if (operator == "/") {
+        result = divide(num1, num2);
+    } else if (operator == "*") {
+        result = multiply(num1, num2);
+    } else if (operator == "+") {
+        result = add(num1, num2);
+    } else if (operator == "-") {
+        result = subtract(num1, num2);
     }
 }
+
+function addEventListenersToButtons() {
+
+    const buttons = document.querySelectorAll(".number-button");
+    const buttonArray = Array.from(buttons);
+    const display = document.querySelector("#calculator-screen");
+    let numCount = 0;
+
+    buttonArray.map((button) => {
+        button.addEventListener("click", () => {
+            if (numCount != 12) {
+                if (display.textContent === "0") {
+                    display.textContent = "";
+                }
+                display.textContent += button.textContent;
+                numCount++;
+            }
+        });
+    });
+
+
+}
+
+addEventListenersToButtons();
